4d602646a5313dfa42e9053bf382c41694cab3eb
Claus Due <claus@wildside.dk>
2013-03-10 14:00:34 +0100

    [TASK] Use Flux autoload registry builder

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,14 +1,3 @@
 <?php
-
-$extensionClassesPath = t3lib_extMgm::extPath('fluidpages') . 'Classes/';
-return array(
-	'tx_fluidpages_backend_backendlayout' => $extensionClassesPath . 'Backend/BackendLayout.php',
-	'tx_fluidpages_backend_pagelayoutselector' => $extensionClassesPath . 'Backend/PageLayoutSelector.php',
-	'tx_fluidpages_controller_pagecontroller' => $extensionClassesPath . 'Controller/PageController.php',
-	'tx_fluidpages_override_backend_view_backendlayoutview' => $extensionClassesPath . 'Override/Backend/View/BackendLayoutView.php',
-	'tx_fluidpages_override_backend_view_pagelayoutview' => $extensionClassesPath . 'Override/Backend/View/PageLayoutView.php',
-	'tx_fluidpages_provider_pageconfigurationprovider' => $extensionClassesPath . 'Provider/PageConfigurationProvider.php',
-	'tx_fluidpages_service_configurationservice' => $extensionClassesPath . 'Service/ConfigurationService.php',
-	'tx_fluidpages_service_pageservice' => $extensionClassesPath . 'Service/PageService.php',
-);
-?>
+require_once t3lib_extMgm::extPath('flux', 'Classes/Utility/Autoload.php');
+return Tx_Flux_Utility_Autoload::getAutoloadRegistryForExtension('fluidpages');
