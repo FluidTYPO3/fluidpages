f1fba2dd57566f5d60c881df5e4955f1d76e7e33
Claus Due <claus@wildside.dk>
2013-03-28 20:41:38 +0100

    [FEATURE] Detect and use Page Controller associated with collection
    This change enables a new breed of Controller: the PageController. If such a Controller exists in a package which provides page templates, FluidPages tries to use methods on that Controller to execute a sub-request where each method on the Controller corresponds to a page template which can be selected in page properties.
    
    The methods will only be attempted if they do in fact exist and errors are caught, logged for debugging and the "errorAction" of FluidPages' own PageController is called (but renders the Error.html template from the associated extension if possible).
    
    Use this to be able to…
    
    * Catch and react to Exceptions on a page level including every possible Exception thrown by plugins in use on the page. If the error is considered fatal, display an error page. If you need to, use advanced error messages.
    * Insert template variables from an actual ControllerContext, for example various aspects of the Request being rendered.
    * Have a proper ControllerContext which points to your carrier extension instead of, as is default behaviour, pointing to FluidPages.
    * Accept and use URL parameters for your page.
    * Use Repositories (not just from your own extension!)
    * Detect user logins (using FrontendUserRepository) and change rendering
    * Everything you're used to doing with Controllers.

--- Classes/Controller/PageController.php
+++ Classes/Controller/PageController.php
@@ -39,13 +39,47 @@ class Tx_Fluidpages_Controller_PageController extends Tx_Fluidpages_Controller_A
 	public function renderAction() {
 		$content = NULL;
 		try {
-			$content = $this->view->render();
+			$row = $this->configurationManager->getContentObject()->data;
+			$extensionKey = $this->provider->getExtensionKey($row);
+			$extensionName = ucfirst(t3lib_div::underscoredToLowerCamelCase($extensionKey));
+			$configuration = $this->pageService->getPageTemplateConfiguration($row['uid']);
+			$action = $configuration['tx_fed_page_controller_action'];
+			$controllerActionName = array_pop(explode('->', $action));
+			$controllerActionName{0} = strtolower($controllerActionName{0});
+			// failure toggles. Instructs ConfigurationService to throw Exceptions when not being able to detect. We capture these and pass to debug.
+			$failHardClass = TRUE;
+			$failHardAction = TRUE;
+			$potentialControllerClassName = $this->configurationService->resolveFluxControllerClassName($action, 'Page', $failHardClass, $failHardAction);
+			if (NULL !== $potentialControllerClassName) {
+				$request = clone $this->request;
+				$request->setControllerExtensionName($extensionName);
+				$request->setControllerActionName($controllerActionName);
+				/** @var $response Tx_Extbase_MVC_Web_Response */
+				$response = $this->objectManager->create('Tx_Extbase_MVC_Web_Response');
+				/** @var $controller Tx_Extbase_Mvc_Controller_ControllerInterface */
+				$controller = $this->objectManager->create($potentialControllerClassName);
+				$content = $controller->processRequest($request, $response);
+			}
 		} catch (Exception $error) {
 			$this->debugService->debug($error);
-			$this->request->setErrors(array('page' => $error));
-			$this->forward('error');
+			$code = $error->getCode();
+			if (1364498093 !== $code && 1364498223 !== $code) {
+				$this->request->setErrors(array('page' => $error));
+				$this->request->setControllerActionName('error');
+				$this->forward('error');
+			}
+			$content = $this->view->render();
 		}
 		return $content;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function errorAction() {
+		$setup = $this->getSetup();
+		$this->view->assign('errors', $this->request->getErrors());
+		$this->view->setTemplatePathAndFilename($setup['templateRootPath'] . 'Page/Error.' . $this->request->getFormat());
 	}
 
 }
