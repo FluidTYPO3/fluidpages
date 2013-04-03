bb691b6704e295cd61ba1349ad5315bfd4296a44
Björn Fromme <mail@bjo3rn.com>
2013-04-03 16:57:07 +0200

    [TASK] Move from locallang_db.xml to locallang.xml

--- ext_tables.php
+++ ext_tables.php
@@ -9,7 +9,7 @@ t3lib_div::loadTCA('pages');
 t3lib_extMgm::addTCAcolumns('pages', array(
 	'tx_fed_page_controller_action' => array (
 		'exclude' => 1,
-		'label' => 'LLL:EXT:fluidpages/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_controller_action',
+		'label' => 'LLL:EXT:fluidpages/Resources/Private/Language/locallang.xml:pages.tx_fed_page_controller_action',
 		'config' => array (
 			'type' => 'user',
 			'userFunc' => 'Tx_Fluidpages_Backend_PageLayoutSelector->renderField'
@@ -17,7 +17,7 @@ t3lib_extMgm::addTCAcolumns('pages', array(
 	),
 	'tx_fed_page_controller_action_sub' => array (
 		'exclude' => 1,
-		'label' => 'LLL:EXT:fluidpages/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_controller_action_sub',
+		'label' => 'LLL:EXT:fluidpages/Resources/Private/Language/locallang.xml:pages.tx_fed_page_controller_action_sub',
 		'config' => array (
 			'type' => 'user',
 			'userFunc' => 'Tx_Fluidpages_Backend_PageLayoutSelector->renderField'
@@ -25,7 +25,7 @@ t3lib_extMgm::addTCAcolumns('pages', array(
 	),
 	'tx_fed_page_flexform' => Array (
 		'exclude' => 1,
-		'label' => 'LLL:EXT:fluidpages/Resources/Private/Language/locallang_db.xml:pages.tx_fed_page_flexform',
+		'label' => 'LLL:EXT:fluidpages/Resources/Private/Language/locallang.xml:pages.tx_fed_page_flexform',
 		'config' => array (
 			'type' => 'flex',
 		)
