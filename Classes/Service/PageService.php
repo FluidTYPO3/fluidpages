8ebc5b7b8808f8e12ca211c6ac61c1dff8950bc9
Claus Due <claus@wildside.dk>
2013-04-25 22:46:44 +0200

    [TASK] Report configured instead of resolved path when path does not exist

--- Classes/Service/PageService.php
+++ Classes/Service/PageService.php
@@ -253,11 +253,11 @@ class Tx_Fluidpages_Service_PageService implements t3lib_Singleton {
 					'paths. This indicates a problem with your TypoScript configuration - most likely a static template is not loaded', t3lib_div::SYSLOG_SEVERITY_WARNING);
 				continue;
 			}
-			$path = $group['templateRootPath'] . 'Page' . '/';
-			$path = t3lib_div::getFileAbsFileName($path);
+			$configuredPath = $group['templateRootPath'] . 'Page' . '/';
+			$path = t3lib_div::getFileAbsFileName($configuredPath);
 			if (FALSE === is_dir($path)) {
 				$this->configurationService->message('The template group "' . $extensionName . '" has been configured to use the templateRootPath "' .
-					$path . '" but this directory does not exist.', t3lib_div::SYSLOG_SEVERITY_FATAL);
+					$configuredPath . '" but this directory does not exist.', t3lib_div::SYSLOG_SEVERITY_FATAL);
 				continue;
 			}
 			$files = scandir($path);
