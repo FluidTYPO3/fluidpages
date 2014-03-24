7ae5b82632fdacdef838141f0d11934ee000d002
Claus Due <claus@namelesscoder.net>
2014-03-24 15:31:52 +0100

    [TASK] Set TYPO3 version dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,9 +34,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5-6.1.99',
+			'typo3' => '4.5.0-6.1.99',
 			'cms' => '',
-			'flux' => '5.0.0',
+			'flux' => '5.0.0-6.0.99',
 		),
 		'conflicts' => array(
 			'templavoila' => '',
