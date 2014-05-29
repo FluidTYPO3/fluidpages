6f69e5cb09b2727ba50bdb8b8607d4487d1b072d
Claus Due <claus@namelesscoder.net>
2014-05-29 16:20:07 +0200

    [TASK] Add default value for remaining SQL columns
    Closes: #171

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,8 +4,8 @@
 CREATE TABLE pages (
 	tx_fluidpages_templatefile varchar(255) DEFAULT NULL,
 	tx_fluidpages_layout varchar(64) DEFAULT NULL,
-	tx_fed_page_flexform text NOT NULL,
-	tx_fed_page_flexform_sub text NOT NULL,
+	tx_fed_page_flexform text DEFAULT '' NOT NULL,
+	tx_fed_page_flexform_sub text DEFAULT '' NOT NULL,
 	tx_fed_page_controller_action varchar(255) DEFAULT '' NOT NULL,
 	tx_fed_page_controller_action_sub varchar(255) DEFAULT '' NOT NULL,
 );
