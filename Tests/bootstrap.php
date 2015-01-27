2f37e135d1f4a6512c381d13f5cbcee0e1e19b52
Claus Due <claus@namelesscoder.net>
2015-01-27 02:52:20 +0100

    [TASK] Update testing strategy

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -6,19 +6,9 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
 	);
 }
 require_once __DIR__ . '/../vendor/autoload.php';
-define('PATH_thisScript', realpath('vendor/typo3/cms/typo3/index.php'));
-define('TYPO3_MODE', 'BE');
-putenv('TYPO3_CONTEXT=Testing');
-$nullCache = array(
-	'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-	'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\NullBackend'
-);
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'] = array(
-	'extbase_object' => $nullCache,
-	'extbase_reflection' => $nullCache
-);
-\TYPO3\CMS\Core\Core\Bootstrap::getInstance()
-	->baseSetup('typo3/')
-	->disableCoreAndClassesCache()
-	->initializeClassLoader()
-	->initializeCachingFramework();
+
+\FluidTYPO3\Development\Bootstrap::initialize(array(
+	'cache_core' => \FluidTYPO3\Development\Bootstrap::CACHE_PHP_NULL,
+	'extbase_object' => \FluidTYPO3\Development\Bootstrap::CACHE_NULL,
+	'extbase_reflection' => \FluidTYPO3\Development\Bootstrap::CACHE_NULL
+));
