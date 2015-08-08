8c086f01d6ae87dd7c8078414cc5d7abc58b03f3
Claus Due <claus@namelesscoder.net>
2015-08-08 18:11:24 +0200

    [TASK] Raise maximum supported Flux version to expected 7.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'typo3' => '6.2.0-7.4.99',
 			'cms' => '',
-			'flux' => '7.2.0-7.2.99',
+			'flux' => '7.2.0-7.3.99',
 		),
 		'conflicts' => array(
 			'templavoila' => '',
