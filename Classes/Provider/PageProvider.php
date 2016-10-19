382f18bdf0f66157d3f1a94a0f75d962fc1ef2eb
Claus Due <claus@namelesscoder.net>
2016-10-19 04:58:19 +0200

    [TASK] Implement early return, remove unused imports

--- Classes/Provider/PageProvider.php
+++ Classes/Provider/PageProvider.php
@@ -19,10 +19,8 @@ use FluidTYPO3\Flux\Utility\RecursiveArrayUtility;
 use FluidTYPO3\Flux\View\TemplatePaths;
 use TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
-use TYPO3\CMS\Core\Utility\DebugUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 /**
  * Page Configuration Provider
@@ -309,7 +307,10 @@ class PageProvider extends AbstractProvider implements ProviderInterface
     {
         if ('update' === $operation) {
             $record = $this->loadRecordFromDatabase($id);
-            if (is_array($record) && $reference->datamap[$this->tableName][$id]) {
+			if (!is_array($record)) {
+				return;
+			}
+            if (isset($reference->datamap[$this->tableName][$id])) {
                 $record = RecursiveArrayUtility::mergeRecursiveOverrule(
                     $record,
                     $reference->datamap[$this->tableName][$id]
