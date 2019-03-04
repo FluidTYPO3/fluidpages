35d7207cb60ceddb02bf9d62aba8e07dff5d3575
Claus Due <claus@namelesscoder.net>
2019-03-04 14:55:05 +0100

    [REMOVAL] Require PHP 7.1 or above
    Disallows PHP 7.0 installs.

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'php' => '7.0.0-7.2.99',
+      'php' => '7.1.0-7.2.99',
       'typo3' => '8.7.0-9.5.99',
       'flux' => '9.0.0-9.99.99',
     ),
